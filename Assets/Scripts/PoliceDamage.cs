045dc89ae8af64ad9c3393a0cf3927b3d2c05119
Honsage <musatov.i.a@edu.mirea.ru>
2026-04-03 22:07:20 +0300

    feat: configure player hud

---
+++ Assets/Scripts/PoliceDamage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceDamage : MonoBehaviour
+{
+    public float damageAmount = 10f;
+    public float damageCooldown = 1f;
+    private float lastDamageTime = 0f;
+    
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (Time.time - lastDamageTime < damageCooldown)
+            return;
+            
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HeroStats heroStats = collision.gameObject.GetComponent<HeroStats>();
+            if (heroStats != null)
+            {
+                heroStats.TakeDamage(damageAmount);
+                lastDamageTime = Time.time;
+                Debug.Log($"Полиция нанесла {damageAmount} урона!");
+            }
+        }
+    }
+}
