2692a77f6ff15257c4e294eeadf95961d28619e8
Honsage <musatov.i.a@edu.mirea.ru>
2026-04-24 18:13:40 +0300

    feat: add keyframe siren lights animation

--- Assets/Scripts/PoliceCar.cs
+++ Assets/Scripts/PoliceCar.cs
@@ -4,18 +4,32 @@ using UnityEngine.AI;
 public class PoliceCar : MonoBehaviour
 {
     public Transform player;
+    public Light redLight;
+    public Light blueLight;
     
     private NavMeshAgent agent;
+    private Animator anim;
     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         
         if (player == null)
         {
             GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
             if (playerObj != null)
                 player = playerObj.transform;
+        }
+
+
+        if (anim != null)
+        {
+            anim.updateMode = AnimatorUpdateMode.Normal;
+            anim.Play("PoliceCar_Siren", 0, 0);
+            anim.speed = 1f;
+            Debug.Log("Animator started playing: " + anim.GetCurrentAnimatorStateInfo(0).length);
+
         }
     }
     
@@ -25,5 +39,13 @@ public class PoliceCar : MonoBehaviour
         {
             agent.SetDestination(player.position);
         }
+
+        if (anim != null)
+        {
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            Debug.Log("Normalized time: " + stateInfo.normalizedTime);
+        }
     }
+
+    
 }
